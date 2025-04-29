daa84b07efa1d02babf8d0a67936eeffbe59f25a
Ayentess <2479277792@qq.com>
2025-04-29 21:12:43 +0800

    算法

--- c++算法.cpp
+++ c++算法.cpp
@@ -143,3 +143,132 @@
 //    }
 //
 //}
+
+//#include<iostream>
+//#include<vector>
+//#include<string>
+//
+//using namespace std;
+//
+//vector<int> add(vector<int> &A, vector<int> &B)
+//{
+//	if (A.size() < B.size())
+//	{
+//		return add(B, A);
+//	}
+//
+//	vector<int> C;
+//
+//	int t = 0;
+//
+//	for (int i = 0; i < A.size(); i++)
+//	{
+//		t += A[i];
+//		if (i < B.size())//如果B还有对应的位
+//		{
+//			t += B[i];
+//		}
+//		C.push_back(t % 10);
+//		t /= 10;
+//	}//循环结束后，t为0，说明没有进位，将C压入C中
+//
+//	if (t)
+//	{
+//		C.push_back(t);
+//	}//如果循环结束后t不为0，说明还有进位，将其压入C中
+//	return C;
+//}
+//
+//int main()
+//{
+//	string a, b;
+//	vector<int> A, B;
+//	cin>>a>>b;
+//
+//	for (int i = a.size() - 1; i >= 0; i--)
+//	{
+//		A.push_back(a[i]-'0');
+//	}
+//
+//	for (int i = b.size() - 1; i >= 0; i--)
+//	{
+//		B.push_back(b[i]-'0');
+//	}
+//
+//	auto C = add(A, B);
+//
+//	for (int i = C.size() - 1; i >= 0; i--)
+//	{
+//		cout << C[i];//倒序输出C中的数字
+//	}
+//
+//	cout << endl;
+//
+//	system("pause");
+//	return 0;
+//}
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool cmp(vector <int>& A, vector <int>& B) 
+{ // 判断是否A ≥ B
+    if (A.size() != B.size())
+    {
+        return A.size() > B.size();
+    }// 如果A、B长度不相同，长度长的那个数大
+    for (int i = A.size() - 1; i >= 0; i--) 
+    { // 否则就要从最高位开始看（因为执行这个函数前，A和B数组都已经倒序，所以这里要从后往前看）
+        if (A[i] != B[i]) return A[i] > B[i]; // 如果A的当前位和B的当前位不相等，当前位大的更大
+    }
+    return true; // 如果A、B数组都相等，这里可以直接返回true，当然也可以直接输出0
+}
+vector <int> sub(vector <int>& A, vector <int>& B) 
+{ // A - B
+    int t = 0; // 每一位上相减得到的数
+    vector <int> C; // 最后的答案
+    for (int i = 0; i < A.size(); i++) 
+    { // 遍历一遍，和高精度加法不一样的是，只要遍历完A就行了，因为这里A肯定比B长
+        t = A[i] - t; // t要等于A的当前位减掉自己，因为上一位有可能出现借位的情况
+        if (i < B.size())
+        {
+            t -= B[i];
+        }// 如果没有遍历完B，那么t减掉B的当前位
+        C.push_back((t + 10) % 10); // 更新C数组
+        // 这里如果没有借位，(t + 10) % 10就刚好等于t
+        // 如果这里有借位，(t + 10) % 10就会借一个10下来
+        if (t < 0) 
+        {
+            t = 1;
+        } // 如果t < 0，说明不够减，需要借位，把t赋值为1，就是在下一次执行中，A的当前位会减掉t
+        else
+        {
+            t = 0;  
+        } // 否则够减，赋值为0，不用借位
+    }
+    while (C.size() > 1 && C.back() == 0) C.pop_back(); // 删除前导0
+    return C; // 返回答案
+}
+int main() 
+{
+    string a, b; // 两个数，因为很大，所以用string来存
+    cin >> a >> b; // 读入
+    vector <int> A, B; // 两个数，因为减法是从最低位开始减，我们可以把两个数倒过来
+    for (int i = a.size() - 1; i >= 0; i--) A.push_back(a[i] - '0'); // 把a数组到过来存入A，记得a是string类型的数组，要减去'0'让它变成数字
+    for (int i = b.size() - 1; i >= 0; i--) B.push_back(b[i] - '0'); // 把b数组倒过来存入B
+    
+    if (cmp(A, B)) 
+    { // 如果A > B
+        auto C = sub(A, B); // 那么可以直接相减
+        for (int i = C.size() - 1; i >= 0; i--) printf("%d", C[i]); // 最后因为C是倒着的，需要反向输出
+    }
+    else 
+    { // 否则A < B，需要计算-(B - A)
+        auto C = sub(B, A); // 计算B - A
+        printf("-"); // 给前面加上一个负号
+        for (int i = C.size() - 1; i >= 0; i--) printf("%d", C[i]); // 反向输出C数组
+    }
+    return 0;
+}
--- c++课堂.cpp
+++ c++课堂.cpp
@@ -1182,162 +1182,162 @@
 //	return 0;
 //}
 
-#include<iostream>
-using namespace std;
-
-class Complex
-{
-public:
-	Complex() {};//构造函数空实现
-	Complex(double real, double image);
-
-	void Print();
-
-	Complex operator+(const Complex& c)
-	{
-		Complex temp;
-		temp.m_real=this->m_real+c.m_real;
-		temp.m_image=this->m_image+c.m_image;
-		return temp;
-	}
-
-	Complex operator-(const Complex& c)
-	{
-		Complex temp;
-		temp.m_real = this->m_real - c.m_real;
-		temp.m_image = this->m_image - c.m_image;
-		return temp;
-	}
-
-	Complex operator*(const Complex& c)
-	{
-		Complex temp;
-		temp.m_real = this->m_real * c.m_real- this->m_image * c.m_image;
-		temp.m_image = this->m_image * c.m_real+ this->m_real * c.m_image;
-		return temp;
-	}
-
-	Complex operator/(const Complex& c)
-	{
-		double numb1;//实部分子
-		double numb2;//虚部分子
-		double denom;
-		numb1 = this->m_real * c.m_real + this->m_image * c.m_image;
-		numb2 = this->m_image * c.m_real - this->m_real * c.m_image;
-        denom = c.m_real * c.m_real + c.m_image * c.m_image;
-
-		Complex temp;
-		temp.m_real = numb1 / denom;
-		temp.m_image = numb2 / denom;
-		return temp;
-	}
-
-
-	double m_real;
-	double m_image;
-};
-
-Complex::Complex(double real, double image)
-{
-	m_real = real;
-	m_image = image;
-}
-
-void Complex::Print()
-{
-	if (m_real == 0 && m_image == 0)
-	{
-		cout << "0" << endl;
-	}
-	else if (m_real == 0 && m_image != 0)
-	{
-		cout << m_image << "i" << endl;
-	}
-	else if (m_real != 0 && m_image == 0)
-	{
-		cout << m_real << endl;
-	}
-	else
-	{
-		cout << m_real << " + " << m_image << "i" << endl;
-	}
-}
-
-void test()
-{
-	double x1, y1;
-	cout << "请输入第一个复数的实部：" << endl;
-	cin >> x1;
-	cout << "请输入第一个复数的虚部：" << endl;
-	cin >> y1;
-	Complex c1(x1, y1);
-
-	double x2, y2;
-	cout << "请输入第二个复数的实部：" << endl;
-	cin >> x2;
-	cout << "请输入第二个复数的虚部：" << endl;
-	cin >> y2;
-	Complex c2(x2, y2);
-
-	Complex c3;
-
-	while (1)
-	{
-		int chioce;
-		cout << "请选择想要实现的方法:" << endl;
-		cout << "1.加法" << endl;
-		cout << "2.减法" << endl;
-		cout << "3.乘法" << endl;
-		cout << "4.除法" << endl;
-        cout << "5.退出" << endl;
-		cin >> chioce;
-
-
-		switch (chioce)
-		{
-			case 1:
-				{
-					c3 = c1 + c2;
-					c3.Print();
-					break;
-				}
-			case 2:
-				{
-					c3 = c1 - c2;
-					c3.Print();
-					break;
-				}
-			case 3:
-				{
-					c3 = c1 * c2;
-					c3.Print();
-					break;
-				}
-			case 4:
-				{
-					c3 = c1 / c2;
-					c3.Print();
-					break;
-				}
-			case 5:
-				{
-					cout << "已退出" << endl;
-					return;
-				}
-			default:
-				{
-					cout << "输入错误" << endl;
-					break;
-				}
-		}
-	}
-}
-
-
-
-int main()
-{
-	test();
-	system("pause");
-	return 0;
-}
+//#include<iostream>
+//using namespace std;
+//
+//class Complex
+//{
+//public:
+//	Complex() {};//构造函数空实现
+//	Complex(double real, double image);
+//
+//	void Print();
+//
+//	Complex operator+(const Complex& c)
+//	{
+//		Complex temp;
+//		temp.m_real=this->m_real+c.m_real;
+//		temp.m_image=this->m_image+c.m_image;
+//		return temp;
+//	}
+//
+//	Complex operator-(const Complex& c)
+//	{
+//		Complex temp;
+//		temp.m_real = this->m_real - c.m_real;
+//		temp.m_image = this->m_image - c.m_image;
+//		return temp;
+//	}
+//
+//	Complex operator*(const Complex& c)
+//	{
+//		Complex temp;
+//		temp.m_real = this->m_real * c.m_real- this->m_image * c.m_image;
+//		temp.m_image = this->m_image * c.m_real+ this->m_real * c.m_image;
+//		return temp;
+//	}
+//
+//	Complex operator/(const Complex& c)
+//	{
+//		double numb1;//实部分子
+//		double numb2;//虚部分子
+//		double denom;
+//		numb1 = this->m_real * c.m_real + this->m_image * c.m_image;
+//		numb2 = this->m_image * c.m_real - this->m_real * c.m_image;
+//        denom = c.m_real * c.m_real + c.m_image * c.m_image;
+//
+//		Complex temp;
+//		temp.m_real = numb1 / denom;
+//		temp.m_image = numb2 / denom;
+//		return temp;
+//	}
+//
+//
+//	double m_real;
+//	double m_image;
+//};
+//
+//Complex::Complex(double real, double image)
+//{
+//	m_real = real;
+//	m_image = image;
+//}
+//
+//void Complex::Print()
+//{
+//	if (m_real == 0 && m_image == 0)
+//	{
+//		cout << "0" << endl;
+//	}
+//	else if (m_real == 0 && m_image != 0)
+//	{
+//		cout << m_image << "i" << endl;
+//	}
+//	else if (m_real != 0 && m_image == 0)
+//	{
+//		cout << m_real << endl;
+//	}
+//	else
+//	{
+//		cout << m_real << " + " << m_image << "i" << endl;
+//	}
+//}
+//
+//void test()
+//{
+//	double x1, y1;
+//	cout << "请输入第一个复数的实部：" << endl;
+//	cin >> x1;
+//	cout << "请输入第一个复数的虚部：" << endl;
+//	cin >> y1;
+//	Complex c1(x1, y1);
+//
+//	double x2, y2;
+//	cout << "请输入第二个复数的实部：" << endl;
+//	cin >> x2;
+//	cout << "请输入第二个复数的虚部：" << endl;
+//	cin >> y2;
+//	Complex c2(x2, y2);
+//
+//	Complex c3;
+//
+//	while (1)
+//	{
+//		int chioce;
+//		cout << "请选择想要实现的方法:" << endl;
+//		cout << "1.加法" << endl;
+//		cout << "2.减法" << endl;
+//		cout << "3.乘法" << endl;
+//		cout << "4.除法" << endl;
+//        cout << "5.退出" << endl;
+//		cin >> chioce;
+//
+//
+//		switch (chioce)
+//		{
+//			case 1:
+//				{
+//					c3 = c1 + c2;
+//					c3.Print();
+//					break;
+//				}
+//			case 2:
+//				{
+//					c3 = c1 - c2;
+//					c3.Print();
+//					break;
+//				}
+//			case 3:
+//				{
+//					c3 = c1 * c2;
+//					c3.Print();
+//					break;
+//				}
+//			case 4:
+//				{
+//					c3 = c1 / c2;
+//					c3.Print();
+//					break;
+//				}
+//			case 5:
+//				{
+//					cout << "已退出" << endl;
+//					return;
+//				}
+//			default:
+//				{
+//					cout << "输入错误" << endl;
+//					break;
+//				}
+//		}
+//	}
+//}
+//
+//
+//
+//int main()
+//{
+//	test();
+//	system("pause");
+//	return 0;
+//}
