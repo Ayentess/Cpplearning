00512afa8080bd4436fc07baa420baaba6ebaaf7
Ayentess <2479277792@qq.com>
2025-05-01 11:41:14 +0800

    算法

--- c++算法.cpp
+++ c++算法.cpp
@@ -208,67 +208,343 @@
 //	return 0;
 //}
 
-#include <iostream>
-#include <vector>
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//bool cmp(vector <int>& A, vector <int>& B) 
+//{ // 判断是否A ≥ B
+//    if (A.size() != B.size())
+//    {
+//        return A.size() > B.size();
+//    }// 如果A、B长度不相同，长度长的那个数大
+//    for (int i = A.size() - 1; i >= 0; i--) 
+//    { // 否则就要从最高位开始看（因为执行这个函数前，A和B数组都已经倒序，所以这里要从后往前看）
+//        if (A[i] != B[i]) return A[i] > B[i]; // 如果A的当前位和B的当前位不相等，当前位大的更大
+//    }
+//    return true; // 如果A、B数组都相等，这里可以直接返回true，当然也可以直接输出0
+//}
+//vector <int> sub(vector <int>& A, vector <int>& B) 
+//{ // A - B
+//    int t = 0; // 每一位上相减得到的数
+//    vector <int> C; // 最后的答案
+//    for (int i = 0; i < A.size(); i++) 
+//    { // 遍历一遍，和高精度加法不一样的是，只要遍历完A就行了，因为这里A肯定比B长
+//        t = A[i] - t; // t要等于A的当前位减掉自己，因为上一位有可能出现借位的情况
+//        if (i < B.size())
+//        {
+//            t -= B[i];
+//        }// 如果没有遍历完B，那么t减掉B的当前位
+//        C.push_back((t + 10) % 10); // 更新C数组
+//        // 这里如果没有借位，(t + 10) % 10就刚好等于t
+//        // 如果这里有借位，(t + 10) % 10就会借一个10下来
+//        if (t < 0) 
+//        {
+//            t = 1;
+//        } // 如果t < 0，说明不够减，需要借位，把t赋值为1，就是在下一次执行中，A的当前位会减掉t
+//        else
+//        {
+//            t = 0;  
+//        } // 否则够减，赋值为0，不用借位
+//    }
+//    while (C.size() > 1 && C.back() == 0) C.pop_back(); // 删除前导0
+//    return C; // 返回答案
+//}
+//int main() 
+//{
+//    string a, b; // 两个数，因为很大，所以用string来存
+//    cin >> a >> b; // 读入
+//    vector <int> A, B; // 两个数，因为减法是从最低位开始减，我们可以把两个数倒过来
+//    for (int i = a.size() - 1; i >= 0; i--) A.push_back(a[i] - '0'); // 把a数组到过来存入A，记得a是string类型的数组，要减去'0'让它变成数字
+//    for (int i = b.size() - 1; i >= 0; i--) B.push_back(b[i] - '0'); // 把b数组倒过来存入B
+//    
+//    if (cmp(A, B)) 
+//    { // 如果A > B
+//        auto C = sub(A, B); // 那么可以直接相减
+//        for (int i = C.size() - 1; i >= 0; i--) printf("%d", C[i]); // 最后因为C是倒着的，需要反向输出
+//    }
+//    else 
+//    { // 否则A < B，需要计算-(B - A)
+//        auto C = sub(B, A); // 计算B - A
+//        printf("-"); // 给前面加上一个负号
+//        for (int i = C.size() - 1; i >= 0; i--) printf("%d", C[i]); // 反向输出C数组
+//    }
+//    return 0;
+//}
+
+//#include<iostream>
+//#include<vector>
+//#include<string>
+//
+//using namespace std;
+//
+//vector<int> sub(vector<int>& A, vector<int>& B)
+//{
+//	vector<int>C;
+//	int t = 0;
+//
+//	for (int i = 0; i < A.size(); i++)
+//	{
+//		t = A[i] - t;
+//		if (i < B.size())
+//		{
+//			t -= B[i];
+//		}
+//		C.push_back((t + 10) % 10);
+//		if (t < 0)
+//		{
+//			t = 1;
+//		}
+//		else
+//		{
+//			t = 0;
+//		}
+//	}
+//
+//	while (C.size() > 1 && C.back() == 0)
+//	{
+//		C.pop_back();
+//	}
+//	return C;
+//}
+//
+//bool cmp(vector<int>& A, vector<int>& B)//判断是否A>=B
+//{
+//	if (A.size() != B.size())
+//	{
+//		return A.size() > B.size();
+//	}
+//
+//	for (int i = A.size() - 1; i >= 0; i--)
+//	{
+//		if (A[i] != B[i])
+//		{
+//			return A[i] > B[i];
+//		}
+//	}
+//
+//	return true;
+//}
+//
+//void test()
+//{
+//	string a, b;
+//	vector<int>A, B;
+//	cin >> a >> b;
+//
+//	for (int i = a.size() - 1; i >= 0; i--)
+//	{
+//		A.push_back(a[i] - '0');
+//	}
+//
+//	for (int i = b.size() - 1; i >= 0; i--)
+//	{
+//		B.push_back(b[i] - '0');
+//	}
+//
+//	if (cmp(A, B))
+//	{
+//		auto C = sub(A, B);
+//		for (int i = C.size()-1; i >= 0; i--)
+//		{
+//			cout << C[i];
+//		}
+//		cout<<endl;
+//	}
+//	else
+//	{
+//		auto C = sub(B, A);
+//		cout << "-";
+//		for (int i = C.size()-1; i >= 0; i--)
+//		{
+//			cout << C[i];
+//		}
+//		cout << endl;
+//	}
+//}
+//
+//int main()
+//{
+//	test();
+//	system("pause");
+//	return 0;
+//}
+//
+
+//#include<iostream>
+//#include<string>
+//#include<vector>
+//
+//using namespace std;
+//
+//vector<int> mul(vector<int>& A, int b)
+//{
+//    vector<int> C;
+//    int t = 0;
+//
+//    for (int i = 0; i < A.size() || t; i++)
+//    {
+//        if (i < A.size())
+//        {
+//            t += A[i] * b;
+//        }
+//        C.push_back(t % 10);
+//        t /= 10;
+//    }
+//
+//    while (C.size() > 1 && C.back() == 0)
+//    {
+//        C.pop_back();
+//    }
+//    return C;
+//}
+//
+//void test()
+//{
+//    string a;
+//    int b;
+//
+//    cin >> a >> b;
+//
+//    vector<int>A;
+//
+//    for (int i = a.size() - 1; i >= 0; i--)
+//    {
+//        A.push_back(a[i] - '0');
+//    }
+//
+//    auto C = mul(A, b);
+//
+//    for (int i = C.size() - 1; i >= 0; i--)
+//    {
+//        cout << C[i];
+//    }
+//    cout << endl;
+//}
+//
+//int main()
+//{
+//    test();
+//    system("pause");
+//    return 0;
+//}
+
+//#include<iostream>
+//#include<string>
+//#include<vector>
+//
+//using namespace std;
+//
+//vector<int> div(vector<int>& A, int b,int &r)
+//{
+//    vector<int> C;
+//    int r = 0;
+//
+//    for (int i = A.size(); i >= 0; i--)
+//    {
+//        r = r * 10 + A[i];
+//        C.push_back(r / b);
+//        r %= b;
+//    }
+//
+//    reverse(C.begin(), C.end());
+//
+//    while (C.size() > 1 && C.back() == 0)
+//    {
+//        C.pop_back();
+//    }
+//    return C;
+//}
+//
+//void test()
+//{
+//    string a;
+//    int b;
+//
+//    cin >> a >> b;
+//
+//    vector<int>A;
+//
+//    for (int i = a.size() - 1; i >= 0; i--)
+//    {
+//        A.push_back(a[i] - '0');
+//    }
+//
+//    int r;
+//    auto C = div(A, b,r);
+//
+//    for (int i = C.size() - 1; i >= 0; i--)
+//    {
+//        cout << C[i];
+//    }
+//    cout << endl;
+//
+//    cout << r << endl;
+//}
+//
+//int main()
+//{
+//    test();
+//    system("pause");
+//    return 0;
+//}
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
 
 using namespace std;
 
-bool cmp(vector <int>& A, vector <int>& B) 
-{ // 判断是否A ≥ B
-    if (A.size() != B.size())
-    {
-        return A.size() > B.size();
-    }// 如果A、B长度不相同，长度长的那个数大
-    for (int i = A.size() - 1; i >= 0; i--) 
-    { // 否则就要从最高位开始看（因为执行这个函数前，A和B数组都已经倒序，所以这里要从后往前看）
-        if (A[i] != B[i]) return A[i] > B[i]; // 如果A的当前位和B的当前位不相等，当前位大的更大
-    }
-    return true; // 如果A、B数组都相等，这里可以直接返回true，当然也可以直接输出0
-}
-vector <int> sub(vector <int>& A, vector <int>& B) 
-{ // A - B
-    int t = 0; // 每一位上相减得到的数
-    vector <int> C; // 最后的答案
-    for (int i = 0; i < A.size(); i++) 
-    { // 遍历一遍，和高精度加法不一样的是，只要遍历完A就行了，因为这里A肯定比B长
-        t = A[i] - t; // t要等于A的当前位减掉自己，因为上一位有可能出现借位的情况
-        if (i < B.size())
-        {
-            t -= B[i];
-        }// 如果没有遍历完B，那么t减掉B的当前位
-        C.push_back((t + 10) % 10); // 更新C数组
-        // 这里如果没有借位，(t + 10) % 10就刚好等于t
-        // 如果这里有借位，(t + 10) % 10就会借一个10下来
-        if (t < 0) 
-        {
-            t = 1;
-        } // 如果t < 0，说明不够减，需要借位，把t赋值为1，就是在下一次执行中，A的当前位会减掉t
-        else
-        {
-            t = 0;  
-        } // 否则够减，赋值为0，不用借位
-    }
-    while (C.size() > 1 && C.back() == 0) C.pop_back(); // 删除前导0
-    return C; // 返回答案
-}
-int main() 
+vector<int> div(vector<int>& A, int b, int& r)
 {
-    string a, b; // 两个数，因为很大，所以用string来存
-    cin >> a >> b; // 读入
-    vector <int> A, B; // 两个数，因为减法是从最低位开始减，我们可以把两个数倒过来
-    for (int i = a.size() - 1; i >= 0; i--) A.push_back(a[i] - '0'); // 把a数组到过来存入A，记得a是string类型的数组，要减去'0'让它变成数字
-    for (int i = b.size() - 1; i >= 0; i--) B.push_back(b[i] - '0'); // 把b数组倒过来存入B
-    
-    if (cmp(A, B)) 
-    { // 如果A > B
-        auto C = sub(A, B); // 那么可以直接相减
-        for (int i = C.size() - 1; i >= 0; i--) printf("%d", C[i]); // 最后因为C是倒着的，需要反向输出
+    vector<int>C;
+    r = 0;
+
+    for (int i = A.size() - 1; i >= 0; i--)
+    {
+        r = r * 10 + A[i];
+        C.push_back(r / b);
+        r %= b;
     }
-    else 
-    { // 否则A < B，需要计算-(B - A)
-        auto C = sub(B, A); // 计算B - A
-        printf("-"); // 给前面加上一个负号
-        for (int i = C.size() - 1; i >= 0; i--) printf("%d", C[i]); // 反向输出C数组
+
+    reverse(C.begin(), C.end());
+
+    while (C.size() > 1 && C.back() == 0)
+    {
+        C.pop_back();
     }
+    return C;
+}
+
+void test()
+{
+    string a;
+    int b;
+    cin >> a >> b;
+
+    vector<int>A;
+
+    for (int i = a.size() - 1; i >= 0; i--)
+    {
+        A.push_back(a[i] - '0');
+    }
+
+    int r;
+    auto C = div(A, b, r);
+
+    for (int i = C.size()-1; i >= 0; i--)
+    {
+        cout << C[i];
+    }
+    cout << endl;
+    cout << r;
+}
+
+int main()
+{
+    test();
+    system("pause");
     return 0;
 }
